de7e71202ced12dfd3d3a77aa452d448e11f19d1
chojnicki <lukchojnicki@gmail.com>
2021-01-27 23:12:01 +0100

    Upgrade Phpunit for PHP 8

--- tests/VideoPlatformsParserTest.php
+++ tests/VideoPlatformsParserTest.php
@@ -6,7 +6,7 @@ use Chojnicki\VideoPlatformsParser\VideoPlatformsParser;
 
 class VideoPlatformsParserTest extends TestCase
 {
-    protected function setUp()
+    protected function setUp():void
     {
         parent::setUp();
     }
